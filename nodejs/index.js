be65b52424d8e32b6c392855ffe63d3e0991ed38
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-08 20:53:28 -0500

    Wrapping everything inside a promise for later usage, if needed.

--- nodejs/index.js
+++ nodejs/index.js
@@ -45,126 +45,122 @@ const glicko2 = require('glicko2');
 const glickoTwo = require('glicko-two');
 const glicko2JS = require('glicko2-js');
 
-// TODO: Make these benchmarks in a promise so we can safely conn.close() after benchmarks have finished.
-for(let i = 1; i < 4; i++) {
-    bench(`go-glicko rating period #${i}`, async (b) => {
-        b.start();
-
-        let players = new Map();
-        let period = new GoGlicko.Period();
+function start() {
+    return new Promise(async (resolve, reject) => {
         let dPlayers = await Player.findAll();
-        
-        for(dP in dPlayers) {
-            let gP = new GoGlicko.Player(new GoGlicko.Rating(dP['rating'], dP['rd'], dP['sigma']));
-            players.set(dP['id'], gP);
-            period.addPlayer(gP);
-        }
-
         let dMatches = await Match.findAll();
-
-        for(dM in dMatches) {
-            period.addMatch(players.get(dM['player1']), players.get(dM['player2']), dM['score']);
-        }
-
-        period.Calculate();
-
-        b.end();
-    });
-}
-
-for(let i = 1; i < 4; i++) {
-    bench(`glicko2 rating period #${i}`, async (b) => {
-        b.start();
-
-        let g2 = new glicko2.Glicko2({
-            tau : 0.5,
-            rating : 1500,
-            rd : 200,
-            vol : 0.06
-        });
-
-        let players = new Map();
-        let matches = [];
-        let dPlayers = await Player.findAll();
+    
+        for(let i = 1; i < 4; i++) {
+            bench(`go-glicko rating period #${i}`, async (b) => {
+                b.start();
         
-        for(dP in dPlayers) {
-            let gP = g2.makePlayer(dP['rating'], dP['rd'], dP['sigma']);
-            players.set(dP['id'], gP);
-        }
-
-        let dMatches = await Match.findAll();
-
-        for(dM in dMatches) {
-            matches.push([players.get(dM['player1']), players.get(dM['player2']), dM['score']]);
-        }
-
-        g2.updateRatings(matches);
-
-        b.end();
-    });
-}
-
-for(let i = 1; i < 4; i++) {
-    bench(`glicko2 rating period #${i}`, async (b) => {
-        b.start();
-
-        let players = new Map();
-        let dPlayers = await Player.findAll();
+                let players = new Map();
+                let period = new GoGlicko.Period();
+                
+                for(dP in dPlayers) {
+                    let gP = new GoGlicko.Player(new GoGlicko.Rating(dPlayers[dP]['rating'], dPlayers[dP]['rd'], dPlayers[dP]['sigma']));
+                    players.set(dPlayers[dP]['id'], gP);
+                    period.addPlayer(gP);
+                }
+    
+                for(dM in dMatches) {
+                    period.addMatch(players.get(dMatches[dM]['player1']), players.get(dMatches[dM]['player2']), dMatches[dM]['score']);
+                }
         
-        for(dP in dPlayers) {
-            let gP = new glickoTwo.Player({
-                rating: dP['rating'],
-                ratingDeviation: dP['rd'],
-                tau: 0.05,
-                volatility: dP['sigma']
+                period.Calculate();
+        
+                b.end();
             });
-
-            players.set(dP['id'], gP);
         }
-
-        let dMatches = await Match.findAll();
-
-        for(dM in dMatches) {
-            players.get(dM['player1']).addResult(players.get(dM['player2']), dM['score']);
+        
+        for(let i = 1; i < 4; i++) {
+            bench(`glicko2 rating period #${i}`, async (b) => {
+                b.start();
+        
+                let g2 = new glicko2.Glicko2({
+                    tau : 0.5,
+                    rating : 1500,
+                    rd : 200,
+                    vol : 0.06
+                });
+        
+                let players = new Map();
+                let matches = [];
+                
+                for(dP in dPlayers) {
+                    let gP = g2.makePlayer(dPlayers[dP]['rating'], dPlayers[dP]['rd'], dPlayers[dP]['sigma']);
+                    players.set(dPlayers[dP]['id'], gP);
+                }
+    
+                for(dM in dMatches) {
+                    matches.push([players.get(dMatches[dM]['player1']), players.get(dMatches[dM]['player2']), dMatches[dM]['score']]);
+                }
+        
+                g2.updateRatings(matches);
+        
+                b.end();
+            });
         }
+        
+        for(let i = 1; i < 4; i++) {
+            bench(`glicko-two rating period #${i}`, async (b) => {
+                b.start();
+        
+                let players = new Map();
+                
+                for(dP in dPlayers) {
+                    let gP = new glickoTwo.Player({
+                        rating: dPlayers[dP]['rating'],
+                        ratingDeviation: dPlayers[dP]['rd'],
+                        tau: 0.05,
+                        volatility: dPlayers[dP]['sigma']
+                    });
+        
+                    players.set(dPlayers[dP]['id'], gP);
+                }
+    
+                for(dM in dMatches) {
+                    players.get(dMatches[dM]['player1']).addResult(players.get(dMatches[dM]['player2']), dMatches[dM]['score']);
+                }
+        
+                players.forEach((player, key) => {
+                    player.updateRating();
+                });
+        
+                b.end();
+            });
+        }
+        
+        for(let i = 1; i < 4; i++) {
+            bench(`glicko2-js rating period #${i}`, async (b) => {
+                b.start();
+                let ratings = new glicko2JS();
 
-        players.forEach((player, key) => {
-            player.updateRating();
-        });
-
-        b.end();
+                for(dP in dPlayers) {
+                    ratings.addPlayer(dPlayers[dP]['id'], dPlayers[dP]['rating'], dPlayers[dP]['rd'], dPlayers[dP]['sigma']);
+                }
+                for(dM in dMatches) {
+                    switch(dMatches[dM]['score']) {
+                        case 1.0:
+                            ratings.addMatch(dMatches[dM]['player2'], dMatches[dM]['player1']);
+                            break;
+                        case 0.5:
+                            ratings.addMatch(dMatches[dM]['player1'], dMatches[dM]['player2'], true);
+                            break;
+                        case 0.0:
+                            ratings.addMatch(dMatches[dM]['player1'], dMatches[dM]['player2']);
+                            break;
+                    }
+                }
+        
+                ratings.calculateRankings();
+        
+                b.end();
+            });
+        }
+    
+        resolve();
     });
 }
 
-for(let i = 1; i < 4; i++) {
-    bench(`glicko2 rating period #${i}`, async (b) => {
-        b.start();
-
-        let dPlayers = await Player.findAll();
-        let ratings = new glicko2JS();
-
-        for(dP in dPlayers) {
-            ratings.addPlayer(dP['id'], dP['rating'], dP['rd'], dP['sigma']);
-        }
-
-        let dMatches = await Match.findAll();
-
-        for(dM in dMatches) {
-            switch(dM['score']) {
-                case 1.0:
-                    ratings.addMatch(dM['player2'], dM['player1']);
-                    break;
-                case 0.5:
-                    ratings.addMatch(dM['player1'], dM['player2'], true);
-                    break;
-                case 0.0:
-                    ratings.addMatch(dM['player1'], dM['player2']);
-                    break;
-            }
-        }
-
-        ratings.calculateRankings();
-
-        b.end();
-    });
-}
+start();
