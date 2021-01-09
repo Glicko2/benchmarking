ac3fb956684630017db6fabd3d3b0edaeac829e7
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-08 19:07:15 -0500

    Adding new update with new benchmarks.

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,65 +1,170 @@
 const bench = require('nanobench');
-const mysql = require('mysql');
+const { Sequelize, DataTypes } = require('sequelize');
+require('sequelize-batches');
 
 let username = process.env.USERNAME;
 let password = process.env.PASSWORD;
 let host = process.env.HOST;
+let database = process.env.DATABASE;
 
-let conn = mysql.createConnection({
+let conn = new Sequelize({
+    database: database,
+    username: username,
+    password: password,
     host: host,
-    user: username,
-    password: password
+    dialect: 'mysql',
+    logging: false
 });
 
-let databases = [
-    {
-        name: '1kp1km',
-        players: 1000,
-        matches: 1000,
+let Player = conn.define('Player', {
+    rating: {
+        type: DataTypes.FLOAT
     },
-    {
-        name: '100kp100km',
-        players: 100000,
-        matches: 100000,
+    rd: {
+        type: DataTypes.FLOAT
     },
-];
-
-
-conn.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
+    sigma: {
+        type: DataTypes.FLOAT
     }
+}, { timestamps: false, tableName: 'players' });
 
-    let GoGlicko = require('go-glicko');
+let Match = conn.define('Match', {
+    player1: {
+        type: DataTypes.NUMBER
+    },
+    player2: {
+        type: DataTypes.NUMBER
+    },
+    score: {
+        type: DataTypes.NUMBER
+    }
+}, { timestamps: false, tableName: 'matches' });
 
-    databases.forEach((db) => {
-        for (let i = 0; i < 3; i++) {
-            bench(`go-glicko '${db.name}' rating period #${i+1}`, (b) => {
-                b.start();
+const GoGlicko = require('go-glicko');
+const glicko2 = require('glicko2');
+const glickoTwo = require('glicko-two');
+const glicko2JS = require('glicko2-js');
 
-                let players = new Map();
-                let period = new GoGlicko.Period();
+// TODO: Make these benchmarks in a promise so we can safely conn.close() after benchmarks have finished.
+for(let i = 1; i < 4; i++) {
+    bench(`go-glicko rating period #${i}`, async (b) => {
+        b.start();
 
-                conn.query(`SELECT * FROM \`${db.name}\`.players`, (err, r, f) => {
-                    r.forEach((p) => {
-                        let gP = new GoGlicko.Player(new GoGlicko.Rating(p['rating'], p['rd'], p['sigma']));
-                        players.set(p['id'], gP);
-                        period.addPlayer(gP);
-                    });
-
-                    conn.query(`SELECT * FROM \`${db.name}\`.matches`, (err, rM, fM) => {
-                        rM.forEach((m) => {
-                            period.addMatch(players.get(m['player1']), players.get(m['player2']), m['score']);
-                        });
-
-                        
-                        period.Calculate();
-
-                        b.end();
-                    });
-                });
-            });
+        let players = new Map();
+        let period = new GoGlicko.Period();
+        let dPlayers = await Player.findAll();
+        
+        for(dP in dPlayers) {
+            let gP = new GoGlicko.Player(new GoGlicko.Rating(dP['rating'], dP['rd'], dP['sigma']));
+            players.set(dP['id'], gP);
+            period.addPlayer(gP);
         }
+
+        let dMatches = await Match.findAll();
+
+        for(dM in dMatches) {
+            period.addMatch(players.get(dM['player1']), players.get(dM['player2']), dM['score']);
+        }
+
+        period.Calculate();
+
+        b.end();
     });
-});
+}
+
+for(let i = 1; i < 4; i++) {
+    bench(`glicko2 rating period #${i}`, async (b) => {
+        b.start();
+
+        let g2 = new glicko2.Glicko2({
+            tau : 0.5,
+            rating : 1500,
+            rd : 200,
+            vol : 0.06
+        });
+
+        let players = new Map();
+        let matches = [];
+        let dPlayers = await Player.findAll();
+        
+        for(dP in dPlayers) {
+            let gP = g2.makePlayer(dP['rating'], dP['rd'], dP['sigma']);
+            players.set(dP['id'], gP);
+        }
+
+        let dMatches = await Match.findAll();
+
+        for(dM in dMatches) {
+            matches.push([players.get(dM['player1']), players.get(dM['player2']), dM['score']]);
+        }
+
+        g2.updateRatings(matches);
+
+        b.end();
+    });
+}
+
+for(let i = 1; i < 4; i++) {
+    bench(`glicko2 rating period #${i}`, async (b) => {
+        b.start();
+
+        let players = new Map();
+        let dPlayers = await Player.findAll();
+        
+        for(dP in dPlayers) {
+            let gP = new glickoTwo.Player({
+                rating: dP['rating'],
+                ratingDeviation: dP['rd'],
+                tau: 0.05,
+                volatility: dP['sigma']
+            });
+
+            players.set(dP['id'], gP);
+        }
+
+        let dMatches = await Match.findAll();
+
+        for(dM in dMatches) {
+            players.get(dM['player1']).addResult(players.get(dM['player2']), dM['score']);
+        }
+
+        players.forEach((player, key) => {
+            player.updateRating();
+        });
+
+        b.end();
+    });
+}
+
+for(let i = 1; i < 4; i++) {
+    bench(`glicko2 rating period #${i}`, async (b) => {
+        b.start();
+
+        let dPlayers = await Player.findAll();
+        let ratings = new glicko2JS();
+
+        for(dP in dPlayers) {
+            ratings.addPlayer(dP['id'], dP['rating'], dP['rd'], dP['sigma']);
+        }
+
+        let dMatches = await Match.findAll();
+
+        for(dM in dMatches) {
+            switch(dM['score']) {
+                case 1.0:
+                    ratings.addMatch(dM['player2'], dM['player1']);
+                    break;
+                case 0.5:
+                    ratings.addMatch(dM['player1'], dM['player2'], true);
+                    break;
+                case 0.0:
+                    ratings.addMatch(dM['player1'], dM['player2']);
+                    break;
+            }
+        }
+
+        ratings.calculateRankings();
+
+        b.end();
+    });
+}
